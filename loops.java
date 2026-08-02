e588da4e7d7600b4b564a25a56586befefcb1bff
manodharareddykoppula <koppulamanodharareddy.25.csm@anits.edu.in>
2026-08-02 18:55:57 +0530

    Clean up commented-out code in loops.java
    Removed commented-out code for various loops and conditions.

---
+++ loops.java
@@ -0,0 +1,29 @@
+public  class Loops { 
+        public static void main(String[] args) {
+
+
+//  nested for loop
+for (int i = 1; i<=20; i++){
+    for (int j =1; j<=20 ; j++){
+        System.out.println("manodhara");
+    }
+System.out.println("reddy");
+}
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
