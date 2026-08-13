e95f2c4f6fc6948b3ed8f4a91a4934aa4ede4315
manodharareddykoppula <koppulamanodharareddy.25.csm@anits.edu.in>
2026-08-13 22:10:25 +0530

    Update marksandaveragesofastudent.java

--- Record/Week3/marksandaveragesofastudent.java
+++ Record/Week3/marksandaveragesofastudent.java
@@ -2,9 +2,11 @@ public class Marks {
     public static void main(String[] args) {
 
         int total = 0;
+
         for (int i = 0; i < 5; i++) {
             total += Integer.parseInt(args[i]);
         }
+
         double average = total / 5.0;
 
         System.out.println("Total = " + total);
