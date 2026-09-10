97a9a7a66c28326cfaad0d5e6269685f8feb9c74
manodharareddykoppula <koppulamanodharareddy.25.csm@anits.edu.in>
2026-09-10 22:36:26 +0530

    Create NegativeArrayException.java

---
+++ observation/task3/NegativeArrayException.java
@@ -0,0 +1,16 @@
+package exceptionhandling;
+
+
+public class NegativeArraySize {
+    public static void main(String[] args) {
+        try {
+            //size cannot be negative it has to be either 0 or 1,2,-----
+            int size = -2;
+            int[] numbers = new int[size];
+            System.out.println("Array created");
+        }
+        catch (NegativeArraySizeException e) {
+            System.out.println("Array size cannot be negative");
+        }
+    }
+}
