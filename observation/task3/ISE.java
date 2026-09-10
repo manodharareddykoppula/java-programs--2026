81e5685b3f12a749a797dcb680a0c790b41c3b42
manodharareddykoppula <koppulamanodharareddy.25.csm@anits.edu.in>
2026-09-10 22:38:26 +0530

    Create ISE.java

---
+++ observation/task3/ISE.java
@@ -0,0 +1,22 @@
+package exceptionhandling;
+
+class Printer{
+    boolean on = false;
+    void print() {
+        if (!on) {
+            throw new IllegalStateException("Printer is turned off");
+        }
+        System.out.println("Printing...");
+    }
+}
+public class ISE {
+    public static void main(String[] args) {
+        Printer p = new Printer();
+        try {
+            p.print();
+        }
+        catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
