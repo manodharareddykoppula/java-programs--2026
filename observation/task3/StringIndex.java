b0e0340ecaddfd97cb2b30fd196ceca5523f9525
manodharareddykoppula <koppulamanodharareddy.25.csm@anits.edu.in>
2026-09-10 22:31:35 +0530

    Create StringIndex.java

---
+++ observation/task3/StringIndex.java
@@ -0,0 +1,15 @@
+package exceptionhandling;
+
+class StringIndexOutOfBounds {
+    public static void main(String[] args) {
+
+        try {
+            String str = "Java";
+
+            System.out.println(str.charAt(10));
+        }
+        catch (StringIndexOutOfBoundsException e) {
+            System.out.println("String index is out of bounds");
+        }
+    }
+}
